1c95d8925dd3f340bab1fb24a2c8e7f21124c98d
Muthamizhl <muthamizhraja5@gmail.com>
2025-07-25 12:57:43 +0530

    "ECB-5 Implement the code for integrate with auth-service"

--- user-service/src/main/java/com/tvm/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tvm.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
